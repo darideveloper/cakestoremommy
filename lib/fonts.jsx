1744735d8343968ef21f955fb30ae46a1ff35f26
daridev <darideveloper@gmail.com>
2023-06-23 16:27:04 -0600

    chore (fonts): update default fonts

--- lib/fonts.jsx
+++ lib/fonts.jsx
@@ -1,5 +1,5 @@
-import { Ysabeau } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 
-export const regularFont = Ysabeau({ subsets: ['latin'] })
-export const titleFont = Ysabeau({ subsets: ['latin'] })
-export const alternativeFont = Ysabeau({ subsets: ['latin'] })
+export const regularFont = Montserrat({ subsets: ['latin'] })
+export const titleFont = Montserrat({ subsets: ['latin'] })
+export const alternativeFont = Montserrat({ subsets: ['latin'] })
