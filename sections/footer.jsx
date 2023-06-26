4b7726c7b78a3f2429667db9ec56f72b44c0d084
daridev <darideveloper@gmail.com>
2023-06-26 12:48:14 -0600

    feat (footer): separator

--- sections/footer.jsx
+++ sections/footer.jsx
@@ -1,76 +1,100 @@
 import PropTypes from "prop-types"
 import { titleFont, alternativeFont } from "@/lib/fonts"
 import ContactIcon from "@/components/contact-icon"
+import Image from "next/image"
+import Wave from "@/components/wave"
 
-export default function Footer({title, subtitle, phone, copyright, socialIcons}) {
+export default function Footer({ title, subtitle, phone, copyright, socialIcons }) {
 
   return (
     <footer
-      className={`
-        bg-pink
-      `}
+      className={`bg-transparent md:bg-pink`}
     >
+
+      {/* Footer separators */}
+      <Image
+        src="/images/footer-bg.svg"
+        width={450}
+        height={450}
+        alt="Footer background image"
+        className={`
+          w-full -mb-20
+          md:hidden xs:-mb-28
+        `}
+      />
+      <Wave 
+        extraClasses={`hidden md:inline-block fill-white h-10 w-full mb-5`}
+        flip={false}
+        opacity={true}
+      />
+
       <div className={`
+          wrapper-container
+          bg-pink
+          w-full
+        `}>
+        <div className={`
           container 
-          text-center pt-5 text-brown mx-auto
+          text-center pt-0 text-brown mx-auto
         `}>
 
-        <div className={`
+          <div className={`
           widgets
           flex flex-col items-center justify-center
           md:flex-row md:gap-10
         `}>
 
-          <div className="contact-us">
-            <h2
-              className={`
+            <div className="contact-us">
+              <h2
+                className={`
                 ${titleFont.className}
                 text-2xl uppercase
               `}
-            >
-              {title}
-            </h2>
-            <address>
-              <a
-                href="tel:+1-551-250-1566"
-                className="opacity-60"
-              >{phone}</a>
-            </address>
-          </div>
-          <div className="follow-us">
-            <p className={`
+              >
+                {title}
+              </h2>
+              <address>
+                <a
+                  href="tel:+1-551-250-1566"
+                  className="opacity-60"
+                >{phone}</a>
+              </address>
+            </div>
+            <div className="follow-us">
+              <p className={`
               ${alternativeFont.className}
               text-5xl
               my-6
               md:text-3xl md:my-0 md:uppercase md:mb-2
             `}>{subtitle}</p>
 
-            <div className={`
+              <div className={`
               socials
               flex items-center justify-center gap-5
             `}>
-              {socialIcons.map((contact, index) => (
-                <ContactIcon
-                  key={index}
-                  link={contact.link}
-                  icon={contact.icon}
-                />
-              ))}
+                {socialIcons.map((contact, index) => (
+                  <ContactIcon
+                    key={index}
+                    link={contact.link}
+                    icon={contact.icon}
+                  />
+                ))}
+              </div>
             </div>
           </div>
-        </div>
 
-        <small
-          className={`
+          <small
+            className={`
             center
             mt-10 inline-block
             uppercase text-xs
           `}>
-          {copyright}
-          <br />
-          Powered by <a href="https://www.darideveloper.com/">Dari Developer</a>
-        </small>
+            {copyright}
+            <br />
+            Powered by <a href="https://www.darideveloper.com/">Dari Developer</a>
+          </small>
 
+        </div>
       </div>
 
     </footer>
