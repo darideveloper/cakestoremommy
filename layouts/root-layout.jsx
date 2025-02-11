0eec5a16ab5e60c9749b820a7ae066b99337df8b
daridev <darideveloper@gmail.com>
2025-02-11 09:37:45 -0600

    feat (root-layout): add my custom tag id

--- layouts/root-layout.jsx
+++ layouts/root-layout.jsx
@@ -7,11 +7,11 @@ import Footer from '@/sections/footer'
 import AOS from 'aos'
 import Head from 'next/head'
 import Script from 'next/script'
-import 'aos/dist/aos.css';
+import 'aos/dist/aos.css'
 
 
-export default function RootLayout({ children, customScroll = false}) {
-  
+export default function RootLayout({ children, customScroll = false }) {
+
   // Page data
   const socialIcon = [
     {
@@ -27,19 +27,19 @@ export default function RootLayout({ children, customScroll = false}) {
       hideDesktop: false,
     },
     {
-      icon: (<path d="M18.48 22.926l-1.193.658c-6.979 3.621-19.082-17.494-12.279-21.484l1.145-.637 3.714 6.467-1.139.632c-2.067 1.245 2.76 9.707 4.879 8.545l1.162-.642 3.711 6.461zm-9.808-22.926l-1.68.975 3.714 6.466 1.681-.975-3.715-6.466zm8.613 14.997l-1.68.975 3.714 6.467 1.681-.975-3.715-6.467z"/>),
+      icon: (<path d="M18.48 22.926l-1.193.658c-6.979 3.621-19.082-17.494-12.279-21.484l1.145-.637 3.714 6.467-1.139.632c-2.067 1.245 2.76 9.707 4.879 8.545l1.162-.642 3.711 6.461zm-9.808-22.926l-1.68.975 3.714 6.466 1.681-.975-3.715-6.466zm8.613 14.997l-1.68.975 3.714 6.467 1.681-.975-3.715-6.467z" />),
       link: "tel:5512501566",
       isLink: false,
       hideDesktop: true,
     },
     {
-      icon: (<path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z"/>),
+      icon: (<path d="M.057 24l1.687-6.163c-1.041-1.804-1.588-3.849-1.587-5.946.003-6.556 5.338-11.891 11.893-11.891 3.181.001 6.167 1.24 8.413 3.488 2.245 2.248 3.481 5.236 3.48 8.414-.003 6.557-5.338 11.892-11.893 11.892-1.99-.001-3.951-.5-5.688-1.448l-6.305 1.654zm6.597-3.807c1.676.995 3.276 1.591 5.392 1.592 5.448 0 9.886-4.434 9.889-9.885.002-5.462-4.415-9.89-9.881-9.892-5.452 0-9.887 4.434-9.889 9.884-.001 2.225.651 3.891 1.746 5.634l-.999 3.648 3.742-.981zm11.387-5.464c-.074-.124-.272-.198-.57-.347-.297-.149-1.758-.868-2.031-.967-.272-.099-.47-.149-.669.149-.198.297-.768.967-.941 1.165-.173.198-.347.223-.644.074-.297-.149-1.255-.462-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.297-.347.446-.521.151-.172.2-.296.3-.495.099-.198.05-.372-.025-.521-.075-.148-.669-1.611-.916-2.206-.242-.579-.487-.501-.669-.51l-.57-.01c-.198 0-.52.074-.792.372s-1.04 1.016-1.04 2.479 1.065 2.876 1.213 3.074c.149.198 2.095 3.2 5.076 4.487.709.306 1.263.489 1.694.626.712.226 1.36.194 1.872.118.571-.085 1.758-.719 2.006-1.413.248-.695.248-1.29.173-1.414z" />),
       link: "https://api.whatsapp.com/send?phone=15512501566",
       isLink: true,
       hideDesktop: false,
     },
   ]
-  
+
   const data = {
     "en": {
       "Header": {
@@ -49,7 +49,7 @@ export default function RootLayout({ children, customScroll = false}) {
           { name: 'Gallery', path: '/#gallery' },
           { name: 'Contact', path: '/#contact' },
           { name: 'Our Flavors', path: '/#flavors' },
-          { name: 'FAQs', path: '/#faqs'}
+          { name: 'FAQs', path: '/#faqs' }
         ]
       },
       "Footer": {
@@ -68,7 +68,7 @@ export default function RootLayout({ children, customScroll = false}) {
           { name: 'Galería', path: '/#gallery' },
           { name: 'Contacto', path: '/#contact' },
           { name: 'Sabores', path: '/#flavors' },
-          { name: 'FAQs', path: '/#faqs'}
+          { name: 'FAQs', path: '/#faqs' }
         ]
       },
       "Footer": {
@@ -98,7 +98,7 @@ export default function RootLayout({ children, customScroll = false}) {
 
   // Get language from context
   const { lang } = useContext(LangContext)
-  
+
   // Get currenbt languaje data
   const dataLang = data[lang]
   const metaLang = meta[lang]
@@ -106,11 +106,12 @@ export default function RootLayout({ children, customScroll = false}) {
 
   // Load AOS
   useEffect(() => {
-    AOS.init();
+    AOS.init()
   }, [])
-  
+
   const content = (
     <>
+      {/* Client google tag */}
       <Script
         strategy="beforeInteractive"
         dangerouslySetInnerHTML={{
@@ -123,14 +124,28 @@ export default function RootLayout({ children, customScroll = false}) {
             console.log("Script executed!");
           `,
         }}
-      />  
-      <Header 
+      />
+
+      {/* My google tag */}
+      <Script
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-NC1DC8B608');
+          `
+        }}
+      />
+
+      <Header
         {...dataLang.Header}
       />
       <main className={regularFont.className}>
         {children}
       </main>
-      <Footer 
+      <Footer
         {...dataLang.Footer}
       />
     </>
@@ -142,26 +157,26 @@ export default function RootLayout({ children, customScroll = false}) {
         <title>{metaLang.title}</title>
         <meta name="description" content={metaLang.description} />
         <meta name="author" content={metaLang.author} />
-        <meta name="keywords" content={metaLang.keywords} />      
+        <meta name="keywords" content={metaLang.keywords} />
       </Head>
       {
         customScroll
-        ?
-        <div 
-          className={`
+          ?
+          <div
+            className={`
             view-scroll
             w-full
             h-screen
             overflow-y-scroll
             mx-auto
           `}
-        >
-          {content}
-        </div>
-        :
-        <div className="mx-auto">
-          {content}          
-        </div>
+          >
+            {content}
+          </div>
+          :
+          <div className="mx-auto">
+            {content}
+          </div>
       }
     </>
   )
