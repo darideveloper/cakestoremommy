1c646245fbe2566cbd8ef3bb69aceb339876fa65
daridev <darideveloper@gmail.com>
2025-02-27 10:36:31 -0600

    feat (root-layout): add client meta pixel

--- layouts/root-layout.jsx
+++ layouts/root-layout.jsx
@@ -139,6 +139,25 @@ export default function RootLayout({ children, customScroll = false }) {
         }}
       />
 
+      {/* CLient meta pixel */}
+      <Script
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '624940397065465');
+            fbq('track', 'PageView');
+          `
+        }}
+      />
+
       <Header
         {...dataLang.Header}
       />
@@ -158,7 +177,19 @@ export default function RootLayout({ children, customScroll = false }) {
         <meta name="description" content={metaLang.description} />
         <meta name="author" content={metaLang.author} />
         <meta name="keywords" content={metaLang.keywords} />
+
+        {/* CLient meta pixel */}
+        <noscript>
+          <img 
+            height="1" 
+            width="1"
+            className='hidden'
+            src="https://www.facebook.com/tr?id=624940397065465&ev=PageView&noscript=1"
+          />
+        </noscript>
+
       </Head>
+      
       {
         customScroll
           ?
